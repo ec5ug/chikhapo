bec2dd4494cf282d140b8f9d311f5853154806b6
Emily Chang <emily.chang@ttic.edu>
2025-11-01 01:13:40 -0500

    functions that help with finding language names and parsing language x out of the language pair

---
+++ utils/languages.py
@@ -0,0 +1,41 @@
+import pycountry
+import re
+import string
+
+
+def convert_iso_to_name(lang):
+    query = pycountry.languages.get(alpha_3=lang)
+    if query:
+        lang_name = query.name
+        lang_name = lang_name.translate(str.maketrans('', '', string.punctuation))
+        return lang_name
+    return ""
+
+def get_direction_of_lang_pair(lang_pair):
+    if lang_pair.endswith("_eng"):
+        return "X_to_eng"
+    elif lang_pair.startswith("eng_"):
+        return "eng_to_X"
+    raise Exception(f"{lang_pair} is invalid as it does not start with eng_ or ends with eng_")
+
+def get_language_from_pair(lang_pair):
+    direction = get_direction_of_lang_pair(lang_pair)
+    if direction == "X_to_eng":
+        return lang_pair.split("_")[0]
+    elif direction == "eng_to_X":
+        return lang_pair.split("_")[1]
+    else:
+        raise Exception("Invalid DIRECTION")
+
+# import unicodedata
+# def remove_accents(text):
+#     return ''.join(
+#         c for c in unicodedata.normalize("NFD", text)
+#         if not unicodedata.combining(c)
+#     )
+# def get_sanitized_language_name(iso): # follows HuggingFace conventions
+#     text = convert_iso_to_name(iso)
+#     text = remove_accents(text)
+#     text = text.replace(" ", "")
+#     text = re.sub(r"[^\w.]", "_", text)
+#     return text
