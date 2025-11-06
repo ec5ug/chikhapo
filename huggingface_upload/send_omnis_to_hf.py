6ca806d84d3cfe4ff2b3bc8410ffce20191f414a
Emily Chang <emily.chang@ttic.edu>
2025-11-06 02:54:17 -0600

    prevent eng_eng from being added

--- huggingface_upload/send_omnis_to_hf.py
+++ huggingface_upload/send_omnis_to_hf.py
@@ -1,8 +1,25 @@
 import json, os, yaml
 from huggingface_hub import HfApi
+from chikhapo.utils.languages import get_language_pair
+valid_langs = set()
+for DIRECTION in ["X_to_eng"]:
+    for filename in os.listdir(f"../omnis/{DIRECTION}"):
+        lang_pair, _ = os.path.splitext(filename)
+        if lang_pair == "eng-eng":
+            continue
+        lang_x = lang_pair.split("-")[0]
+
+        with open(f"../omnis/X_to_eng/{filename}", "r") as f:
+            x_to_eng_lex = json.load(f)
+
+        with open(f"../omnis/eng_to_X/eng-{lang_x}.json", "r") as f:
+            eng_to_x_lex = json.load(f)
+
+        if len(x_to_eng_lex) >= 100:
+            valid_langs.add(lang_x)
 
 REPO_NAME = "ec5ug/chikhapo"
-DATA_DIR = "hf_data/data"
+DATA_DIR = "../hf_data/data"
 os.makedirs(DATA_DIR, exist_ok=True)
 
 configs = []
@@ -10,12 +27,18 @@ langs = set()
 
 for DIRECTION in ["X_to_eng", "eng_to_X"]:
     all_records = [] # for creating all_eng and eng_all
-    for filename in os.listdir(f"omnis/{DIRECTION}"):
-        lang_pair, _ = os.path.splitext(filename)
-        src_lang, tgt_lang = lang_pair.split("-")
+    for lang in valid_langs:
+        if DIRECTION == "X_to_eng":
+            filename = f"{lang}-eng.json"
+        else:
+            filename = f"eng-{lang}.json"
+        if DIRECTION == "X_to_eng":
+            src_lang, tgt_lang = lang, "eng"
+        else:
+            src_lang, tgt_lang = "eng", lang
         config_name = f"{src_lang}_{tgt_lang}"
 
-        with open(f"omnis/{DIRECTION}/{filename}", "r") as f:
+        with open(f"../omnis/{DIRECTION}/{filename}", "r") as f:
             lexicon = json.load(f)
 
         if len(lexicon) < 99:
@@ -57,7 +80,7 @@ for DIRECTION in ["X_to_eng", "eng_to_X"]:
     if DIRECTION=="X_to_eng":
         config_name = "all_eng"
     else:
-        all_filename = "eng_all"
+        config_name = "eng_all"
     
     # for dumping all_eng or eng_all
     output_path = os.path.join(DATA_DIR, f"{config_name}.jsonl")
@@ -72,7 +95,7 @@ for DIRECTION in ["X_to_eng", "eng_to_X"]:
 # creating the YAML based on language and splits
 langs = list(langs)
 print(len(langs))
-yaml_path = os.path.join("dataset_config.yaml")
+yaml_path = os.path.join("../dataset_config.yaml")
 
 with open(yaml_path, "w", encoding="utf-8") as f:
     f.write("\n")
@@ -92,7 +115,7 @@ with open(yaml_path, "w", encoding="utf-8") as f:
 # Upload folder
 api = HfApi()
 api.upload_large_folder(
-    folder_path="hf_data/",
+    folder_path="../hf_data/",
     repo_id=REPO_NAME,
     repo_type="dataset",
 )
