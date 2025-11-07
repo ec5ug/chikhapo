d30cfd2a4bb20008220d1109dd7b63bd143e9047
Emily Chang <emily.chang@ttic.edu>
2025-11-07 11:48:07 -0600

    rename and reformat

--- chikhapo_pkg/tests/test_word_translation_with_context_feeder.py
+++ chikhapo_pkg/tests/test_word_translation_with_context_feeder.py
@@ -2,32 +2,36 @@ import unittest
 from chikhapo import TaskFeeder
 
 class TestWordTranslationWithContextTaskFeeder(unittest.TestCase):
+    def setUp(self):
+        self.feeder = TaskFeeder("word_translation_with_context")
+
     def test_get_word_translation_with_context_data_for_lang_pair_more_than_300_word_sentence_lite_true(self):
-        feeder = TaskFeeder("word_translation_with_context")
-        list_of_words_sentences = feeder.get_data_for_lang_pair("avn_Latn_eng")
-        # self.assertIsInstance(list_of_words_sentences, list)
-        self.assertEqual(len(list_of_words_sentences), 300)
+        words_sentences_translations = self.feeder.get_data_for_lang_pair("avn_Latn_eng")
+        self.assertIsInstance(words_sentences_translations, dict)
+        self.assertEqual(len(words_sentences_translations), 300)
+        self.assertTrue(all(isinstance(k, tuple)) for k in words_sentences_translations.keys())
+        self.assertTrue(all(isinstance(v, list) for v in words_sentences_translations.values()))
         # self.assertTrue(all(isinstance(w, tuple) for w in list_of_words_sentences))
         # self.assertEqual(len(list_of_words_sentences[0]), 2)
 
     def test_get_word_translation_with_context_data_for_lang_pair_more_than_300_word_sentence_lite_false(self):
-        feeder = TaskFeeder("word_translation_with_context")
-        list_of_words_sentences = feeder.get_data_for_lang_pair("avn_Latn_eng", lite=False)
+        words_sentences_translations = self.feeder.get_data_for_lang_pair("avn_Latn_eng", lite=False)
         # self.assertIsInstance(list_of_words_sentences, list)
-        self.assertGreater(len(list_of_words_sentences), 300)
+        self.assertIsInstance(words_sentences_translations, dict)
+        self.assertGreater(len(words_sentences_translations), 300)
+        self.assertTrue(all(isinstance(k, tuple)) for k in words_sentences_translations.keys())
+        self.assertTrue(all(isinstance(v, list) for v in words_sentences_translations.values()))
         # self.assertTrue(all(isinstance(w, tuple) for w in list_of_words_sentences))
         # self.assertEqual(len(list_of_words_sentences[0]), 2)
 
     def test_get_word_translation_with_context_prompts_for_lang_pair_more_than_300_word_sentence_lite_true(self):
-        feeder = TaskFeeder("word_translation_with_context")
-        list_of_prompts = feeder.get_prompts_for_lang_pair("avn_Latn_eng")
+        list_of_prompts = self.feeder.get_prompts_for_lang_pair("avn_Latn_eng")
         self.assertIsInstance(list_of_prompts, list)
         self.assertEqual(len(list_of_prompts), 300)
         self.assertTrue(all(isinstance(w, str) for w in list_of_prompts))
 
     def test_get_word_translation_with_context_prompts_for_lang_pair_more_than_300_word_sentence_lite_false(self):
-        feeder = TaskFeeder("word_translation_with_context")
-        list_of_prompts = feeder.get_prompts_for_lang_pair("avn_Latn_eng")
+        list_of_prompts = self.feeder.get_prompts_for_lang_pair("avn_Latn_eng")
         self.assertIsInstance(list_of_prompts, list)
         self.assertEqual(len(list_of_prompts), 300)
         self.assertTrue(all(isinstance(w, str) for w in list_of_prompts))
